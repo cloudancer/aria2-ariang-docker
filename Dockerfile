2cfebfada8061f8ea7a3b1308a1a429977afd882
vagrant <vagrant@vagrant.vm>
2020-08-14 05:54:33 +0800

    first test

---
+++ Dockerfile
@@ -0,0 +1,47 @@
+FROM alpine:3.12
+
+ENV HTTP_PORT=80
+ENV EXTERNAL_PORT=80
+ENV PUID=1000
+ENV PGID=1000
+#ENV TRACKER_URL=https://cdn.jsdelivr.net/gh/ngosang/trackerslist/trackers_all.txt
+#ENV TRACKER_URL=https://trackerslist.com/best_aria2.txt
+ENV ENABLE_UPDATE_TRACKER=true
+ENV ENABLE_AUTO_RANDOM_ARIA=true
+ENV ENABLE_AUTO_CLEAR_ARIANG=true
+ENV ENABLE_PASSWORD=true
+ENV TZ=Asia/Shanghai
+
+VOLUME ["/data","/conf"]
+
+EXPOSE 80 6881 6888
+
+WORKDIR /app
+ADD app /app
+RUN chmod +x /app/*.sh
+ADD conf /conf
+RUN echo '*/30 * * * * /app/tracker.sh' > /etc/crontabs/root
+CMD /app/run.sh
+HEALTHCHECK --interval=5s --timeout=3s --start-period=5s --retries=3 CMD /app/healthcheck.sh
+#RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
+
+RUN apk add --no-cache \
+    aria2 \
+    wget \
+    apache2-utils \
+    sudo \
+    nginx \
+    curl \
+    tzdata 
+
+RUN ARIAGN_VERSION=`curl --silent "https://api.github.com/repos/mayswind/AriaNg/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'` \
+    && mkdir -p /run/nginx \
+    && mkdir -p /usr/share/nginx/html \
+    && rm -rf /usr/share/nginx/html/* \
+    && wget -N --no-check-certificate https://github.com/mayswind/AriaNg/releases/download/${ARIAGN_VERSION}/AriaNg-${ARIAGN_VERSION}.zip \
+    && unzip AriaNg-${ARIAGN_VERSION}.zip -d /usr/share/nginx/html \
+    && rm -rf AriaNg-${ARIAGN_VERSION}.zip \
+    && echo Set disable_coredump false >> /etc/sudo.conf
+
+RUN apk del \
+    wget
---
+++ app/run.sh
@@ -0,0 +1,139 @@
+#!/bin/sh
+
+# 设置时区
+if [ -n  ${TZ} ]
+then
+    ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime && echo ${TZ} > /etc/timezone
+fi
+
+# 初始化aria2配置
+if [ ! -f /conf/aria2.session ] 
+then
+    touch /conf/aria2.session
+fi
+
+if [ ${ENABLE_AUTO_RANDOM_ARIA} == "true" ] 
+then
+    cp -f /app/random.conf /etc/nginx/conf.d/default.conf
+    # 随机化jsonrcp路径
+    if [ -f oldRpcPath.txt ]
+    then
+        OLD_RPC_PATH=`cat oldRpcPath.txt`
+    else
+        OLD_RPC_PATH="jsonrpc"
+    fi
+    RPC_PATH=`cat /proc/sys/kernel/random/uuid`
+    sed -i 's/location \/'"${OLD_RPC_PATH}"'/location \/'"${RPC_PATH}"'/g' /etc/nginx/conf.d/default.conf
+    sed -i 's/\"'"${OLD_RPC_PATH}"'\"/\"'"${RPC_PATH}"'\"/g' /usr/share/nginx/html/js/aria-ng*.js
+    echo ${RPC_PATH} > oldRpcPath.txt
+
+    # 随机化aria2密钥
+    OLD_ARIA2_TOKEN=`grep "^rpc-secret=" /conf/aria2.conf`
+    ARIA2_TOKEN=`cat /proc/sys/kernel/random/uuid`
+    if [ -z "${OLD_ARIA2_TOKEN}" ]
+    then
+        sed -i '$a rpc-secret='"${ARIA2_TOKEN}" /conf/aria2.conf
+    else
+        sed -i 's/'"${OLD_ARIA2_TOKEN}"'/rpc-secret='"${ARIA2_TOKEN}"'/g' /conf/aria2.conf
+    fi
+    BASE64_ARIA2_TOKEN=`echo -n ${ARIA2_TOKEN} | base64`
+    sed -i 's/secret:\"[^\"]*\",/secret:\"'"${BASE64_ARIA2_TOKEN}"'\",/g' /usr/share/nginx/html/js/aria-ng*.js
+
+    # 修改aria-ng*.js路径，强制重新加载文件。
+    OLD_ARIA_NG_JS=`ls /usr/share/nginx/html/js/aria-ng*.js`
+    OLD_ARIA_NG_JS_FILE=${OLD_ARIA_NG_JS##*/}
+    ARIA_NG_JS="/usr/share/nginx/html/js/aria-ng"`cat /proc/sys/kernel/random/uuid`".js"
+    ARIA_NG_JS_FILE=${ARIA_NG_JS##*/}
+    mv "${OLD_ARIA_NG_JS}" "${ARIA_NG_JS}"
+    sed -i 's/'"${OLD_ARIA_NG_JS_FILE}"'/'"${ARIA_NG_JS_FILE}"'/g' /usr/share/nginx/html/*.*
+
+    # 随机化cookie
+    if [ -f oldCookie.txt ]
+    then
+        OLD_COOKIE=`cat oldCookie.txt`
+    else
+        OLD_COOKIE="webcookiemask"
+    fi
+    COOKIE=`cat /proc/sys/kernel/random/uuid`
+    sed -i 's/'"${OLD_COOKIE}"'/'"${COOKIE}"'/g' /etc/nginx/conf.d/default.conf
+    echo ${COOKIE} > oldCookie.txt
+else
+    if [ ${ENABLE_PASSWORD} == "true" ] 
+    then
+        cp -f /app/default.conf /etc/nginx/conf.d/default.conf
+    else
+        cp -f /app/nopassword.conf /etc/nginx/conf.d/default.conf
+    fi
+
+    # 设置ARIA2密钥
+    OLD_ARIA2_TOKEN=`grep -Eo "^rpc-secret=.*" /conf/aria2.conf | cut -d '=' -f 2`
+    if [[ -z "${OLD_ARIA2_TOKEN}" ]]
+    then
+        ARIA2_TOKEN=`cat /proc/sys/kernel/random/uuid`
+        sed -i '$a rpc-secret='"${ARIA2_TOKEN}" /conf/aria2.conf
+    else
+        ARIA2_TOKEN=${OLD_ARIA2_TOKEN}
+    fi
+    
+    BASE64_ARIA2_TOKEN=`echo -n ${ARIA2_TOKEN} | base64`
+    sed -i 's/secret:\"[^\"]*\",/secret:\"'"${BASE64_ARIA2_TOKEN}"'\",/g' /usr/share/nginx/html/js/aria-ng*.js
+fi
+
+# 修改AriaNg，每次启动时都重新设置页面。
+if [ ${ENABLE_AUTO_CLEAR_ARIANG} == "true" ] 
+then
+    sed -i 's/body class/body onload="localStorage.clear();" class/g' /usr/share/nginx/html/index.html
+fi
+
+# 修改端口号，考虑到重启容器的情况。
+if [ -f oldHttpPort.txt ]
+then
+    OLD_HTTP_PORT=`cat oldHttpPort.txt`
+else
+    OLD_HTTP_PORT=80
+fi
+sed -i 's/'"${OLD_HTTP_PORT}"'/'"${HTTP_PORT}"'/g' /etc/nginx/conf.d/default.conf
+echo ${HTTP_PORT} > oldHttpPort.txt
+
+if [ -f oldExternalPort.txt ]
+then
+    OLD_EXTERNAL_PORT=`cat oldExternalPort.txt`
+else
+    OLD_EXTERNAL_PORT=6800
+fi
+sed -i 's/'"${OLD_EXTERNAL_PORT}"'/'"${EXTERNAL_PORT}"'/g' /usr/share/nginx/html/js/aria-ng*.js
+echo ${EXTERNAL_PORT} > oldExternalPort.txt
+
+# 设置文件权限
+chown -R ${PUID}:${PGID} /conf
+chown -R ${PUID}:${PGID} /data
+touch /var/log/aria2.log
+chmod 755 /var/log/aria2.log
+chown ${PUID}:${PGID} /var/log/aria2.log
+
+# 设置aria2运行用户
+if [ ${PUID} -eq "0" ]
+then
+    USER=root
+else
+    USER=aria2
+    addgroup --gid "$PGID" "$USER"
+    adduser \
+        --disabled-password \
+        --ingroup "$USER" \
+        --no-create-home \
+        --uid "$PUID" \
+        "$USER"
+fi
+
+# 启动nginx
+nginx
+
+# 启动cron，自动更新tracker
+if [ ${ENABLE_UPDATE_TRACKER} == "true" ] 
+then
+    crond
+fi
+
+# 启动aria2
+sudo -u#${PUID} /usr/bin/aria2c --conf-path=/conf/aria2.conf
